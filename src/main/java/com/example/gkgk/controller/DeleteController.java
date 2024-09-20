237f5e855c29debd8591c49d59aa6a524e9cbad9
whkim98 <hyeong7272@daum.net>
2024-09-20 13:40:41 +0900

    delete도 됨

--- src/main/java/com/example/gkgk/controller/DeleteController.java
+++ src/main/java/com/example/gkgk/controller/DeleteController.java
@@ -1,5 +1,6 @@
 package com.example.gkgk.controller;
 
+import com.example.gkgk.ftp.ftpClientUtil;
 import com.example.gkgk.service.itemService;
 import com.example.gkgk.service.updateService;
 //import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class DeleteController {
@@ -23,7 +26,7 @@ public class DeleteController {
     public String delete(int id, @RequestParam String file_name,
                          HttpServletRequest request) {
 
-        String uploadPath = request.getSession().getServletContext().getRealPath("/resources/file");
+        String uploadPath = request.getSession().getServletContext().getRealPath("/resources/ftpFile");
 
         File file = new File(uploadPath + File.separator + file_name);
         if (file.exists()) {
@@ -32,6 +35,22 @@ public class DeleteController {
             } else {
                 System.out.println("파일 삭제 실패: " + file_name);
             }
+        }
+
+        try{
+            ftpClientUtil ftp = new ftpClientUtil("localhost", 21, "whftp", "1234");
+            File oldFile = new File(uploadPath + File.separator + file_name);
+            if (oldFile.exists()) {
+                if (oldFile.delete()) {
+                    System.out.println("이전 파일 삭제 성공: " + file_name);
+                } else {
+                    System.out.println("이전 파일 삭제 실패: " + file_name);
+                }
+            }
+            String remoteFilePath = URLEncoder.encode(("D:/whftp/" + file_name), StandardCharsets.UTF_8);
+            ftp.deleteFile(remoteFilePath);
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
         itemService.deleteItem(id);
