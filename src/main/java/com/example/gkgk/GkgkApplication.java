de6a66d9920163f118cf45e2adac9640f7ef97c1
whkim98 <hyeong7272@daum.net>
2024-09-25 16:14:25 +0900

    외장 apache-tomcat 배포를 위한 상속 추가

--- src/main/java/com/example/gkgk/GkgkApplication.java
+++ src/main/java/com/example/gkgk/GkgkApplication.java
@@ -3,10 +3,17 @@ package com.example.gkgk;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 //@MapperScan("com.example.gkgk.mapper") // 매퍼 인터페이스가 위치한 패키지
-public class GkgkApplication {
+@SpringBootApplication
+public class GkgkApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GkgkApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GkgkApplication.class, args);
